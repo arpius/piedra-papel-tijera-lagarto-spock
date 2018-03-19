738187aa36d5b3c9a151f49c402f7ace4b10dd1e
Aritz <aritz.pius@anche.no>
2018-03-19 21:49:33 +0100

    pasar código a python 3

--- piedraPapelTijeraLagartoSpock.py
+++ piedraPapelTijeraLagartoSpock.py
@@ -1,21 +1,24 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 19 17:40:37 2015
 
-@author: aritz
-"""
 from random import choice
 
 opciones = ["piedra", "papel", "tijera", "lagarto", "spock"]
-jugador = raw_input("Hola forastero, ¿cómo te llamas?\n>> ")
 
-print "\nEncantado de conocerte %s." % str.capitalize(jugador)
-print "\nVamos a jugar a piedra, papel, tijera, lagarto, Spock y si me ganas "
-print "podrás irte a tu casa. Pero de lo contario..."
-print "\n...tendrás que casarte con mi prima. Si, aquella con bigote."
+PIEDRA = opciones[0]
+PAPEL = opciones[1]
+TIJERA = opciones[2]
+LAGARTO = opciones[3]
+SPOCK = opciones[4]
 
-turno = raw_input("\nElige bien tus bazas. Es tu turno:\n>> ")
+jugador = input("Hola forastero, ¿cómo te llamas?\n>> ")
+
+
+def introduccion():
+    print("\nEncantado de conocerte {}.".format(str.capitalize(jugador)))
+    print("Vamos a jugar a piedra, papel, tijera, lagarto, Spock y si me ganas",
+          "podrás irte a tu casa. Pero de lo contario...",
+          "tendrás que casarte con mi prima. Si, aquella con bigote.")
 
 
 def jugada_aleatoria():
@@ -27,70 +30,58 @@ def combate_a_muerte(jugador, contrincante):
     jugador = str.lower(jugador)
     contrincante = str.lower(contrincante)
     victoria = False
-    if jugador == opciones[0] and contrincante == opciones[1]:
+
+    if jugador == PIEDRA and contrincante == PAPEL or jugador == PAPEL and contrincante == PIEDRA:
         resultado = "Papel envuelve piedra"
-    elif jugador == opciones[0] and contrincante == opciones[2]:
-        victoria = True
+    elif jugador == PIEDRA and contrincante == TIJERA or jugador == TIJERA and contrincante == PIEDRA:
         resultado = "Piedra rompe tijera"
-    elif jugador == opciones[0] and contrincante == opciones[3]:
-        victoria = True
+    elif jugador == PIEDRA and contrincante == LAGARTO or jugador == LAGARTO and contrincante == PIEDRA:
         resultado = "Piedra aplasta lagarto"
-    elif jugador == opciones[0] and contrincante == opciones[4]:
+    elif jugador == PIEDRA and contrincante == SPOCK or jugador == SPOCK and contrincante == PIEDRA:
         resultado = "Spock vaporiza piedra"
-    elif jugador == opciones[1] and contrincante == opciones[0]:
-        victoria = True
-        resultado = "Papel envuelve piedra"
-    elif jugador == opciones[1] and contrincante == opciones[2]:
+    elif jugador == PAPEL and contrincante == TIJERA or jugador == TIJERA and contrincante == PAPEL:
         resultado = "Tijera corta papel"
-    elif jugador == opciones[1] and contrincante == opciones[3]:
+    elif jugador == PAPEL and contrincante == LAGARTO or jugador == LAGARTO and contrincante == PAPEL:
         resultado = "Lagarto come papel"
-    elif jugador == opciones[1] and contrincante == opciones[4]:
-        victoria = True
+    elif jugador == PAPEL and contrincante == SPOCK or jugador == SPOCK and contrincante == PAPEL:
         resultado = "Papel refuta Spock"
-    elif jugador == opciones[2] and contrincante == opciones[0]:
-        resultado = "Piedra rompe tijera"
-    elif jugador == opciones[2] and contrincante == opciones[1]:
-        victoria = True
-        resultado = "Tijera corta papel"
-    elif jugador == opciones[2] and contrincante == opciones[3]:
-        victoria = True
+    elif jugador == TIJERA and contrincante == LAGARTO or jugador == LAGARTO and contrincante == TIJERA:
         resultado = "Tijera decapita lagarto"
-    elif jugador == opciones[2] and contrincante == opciones[4]:
+    elif jugador == TIJERA and contrincante == SPOCK or jugador == SPOCK and contrincante == TIJERA:
         resultado = "Spock desintegra tijera"
-    elif jugador == opciones[3] and contrincante == opciones[0]:
-        resultado = "Piedra aplasta lagarto"
-    elif jugador == opciones[3] and contrincante == opciones[1]:
-        victoria = True
-        resultado = "Lagarto come papel"
-    elif jugador == opciones[3] and contrincante == opciones[2]:
-        resultado = "Tijera decapita lagarto"
-    elif jugador == opciones[3] and contrincante == opciones[4]:
-        victoria = True
+    elif jugador == SPOCK and contrincante == LAGARTO or jugador == LAGARTO and contrincante == SPOCK:
         resultado = "Lagarto envenena Spock"
-    elif jugador == opciones[4] and contrincante == opciones[0]:
-        victoria = True
-        resultado = "Spock vaporiza piedra"
-    elif jugador == opciones[4] and contrincante == opciones[1]:
-        resultado = "Papel refuta Spock"
-    elif jugador == opciones[4] and contrincante == opciones[2]:
-        victoria = True
-        resultado = "Spock desintegra tijera"
-    elif jugador == opciones[4] and contrincante == opciones[3]:
-        resultado = "Lagarto envenena Spock"
-    elif (jugador == opciones[0] and contrincante == opciones[0] or
-            jugador == opciones[1] and contrincante == opciones[1] or
-            jugador == opciones[2] and contrincante == opciones[2] or
-            jugador == opciones[3] and contrincante == opciones[3] or
-            jugador == opciones[4] and contrincante == opciones[4]):
+    elif jugador == PIEDRA and contrincante == PIEDRA or \
+            jugador == PAPEL and contrincante == PAPEL or \
+            jugador == TIJERA and contrincante == TIJERA or \
+            jugador == LAGARTO and contrincante == LAGARTO or \
+            jugador == SPOCK and contrincante == SPOCK:
         resultado = "¡Empate! voy al rincón de llorar, espero" \
-                    " que sigas ahí cuando vuelva..."
+                    "que sigas ahí cuando vuelva..."
     else:
         resultado = "Ese movimiento no está permitido."
-    if victoria:
-        return "%s ¡Maldición! he sido derrotado :(" % resultado
-    else:
-        return "%s ¡MWAJAJAJA te casarás con mi prima!" % resultado
 
+    if jugador == PIEDRA and contrincante == TIJERA or \
+            jugador == PIEDRA and contrincante == LAGARTO or \
+            jugador == PAPEL and contrincante == PIEDRA or \
+            jugador == PAPEL and contrincante == SPOCK or \
+            jugador == TIJERA and contrincante == PAPEL or \
+            jugador == TIJERA and contrincante == LAGARTO or \
+            jugador == LAGARTO and contrincante == PAPEL or \
+            jugador == LAGARTO and contrincante == SPOCK or \
+            jugador == SPOCK and contrincante == PIEDRA or \
+            jugador == SPOCK and contrincante == TIJERA:
+        victoria = True
+
+    if victoria:
+        return "{} ¡Maldición! he sido derrotado :(".format(resultado)
+    else:
+        return "{} ¡MWAJAJAJA te casarás con mi prima!".format(resultado)
+
+
+introduccion()
+turno = input("\nElige bien tus bazas. Es tu turno:\n>> ")
 contrincante = jugada_aleatoria()
-print "%s contra %s. " % (str.upper(turno), str.upper(contrincante))
-print combate_a_muerte(turno, contrincante)
+
+print("{} contra {}. ".format(str.upper(turno), str.upper(contrincante)))
+print(combate_a_muerte(turno, contrincante))
